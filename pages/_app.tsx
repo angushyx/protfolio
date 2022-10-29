cd58d1d7762f60e133342139d9512619ffcc1d8a
Angus <94235117+angushyx@users.noreply.github.com>
2022-10-29 10:45:00 +0800

    _app tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { AnimatePresence } from "framer-motion"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import { useRouter } from "next/router"
+import TawkMessengerReact from "@tawk.to/tawk-messenger-react"
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
@@ -13,6 +14,10 @@ function MyApp({ Component, pageProps }: AppProps) {
       <AnimatePresence>
         <Header pathName={router.pathname} />
         <Component {...pageProps} />
+        <TawkMessengerReact
+          propertyId={process.env.NEXT_PUBLIC_TWAKTO_PROPERTY_ID}
+          widgetId={process.env.NEXT_PUBLIC_TWAKTO_WIDGET_ID}
+        />
         <Footer />
       </AnimatePresence>
     </>
