05c25a37aa19ed89541ae6450df23ea252fdf58a
Angus <94235117+angushyx@users.noreply.github.com>
2022-10-29 08:55:24 +0800

    eslint projectId oj

--- pages/work/projects/[projectId].tsx
+++ pages/work/projects/[projectId].tsx
@@ -54,7 +54,7 @@ const project = () => {
 //   return {
 //     paths: [{ params: { id: "1" } }, { params: { id: "2" } }],
 //? true: 當 params = 3 不會回傳 404 而是回傳 fallback 頁面--> fallback 葉面是沙小(上一夜的意思嗎?)
-// ? 當進入沒有定義 params　的頁面時，可以搭配 const router = useRouter(); router.isFallback --> true 函式做 loading 效果
+// ? 當進入沒有定義 params的頁面時，可以搭配 const router = useRouter(); router.isFallback --> true 函式做 loading 效果
 //? false: 當 params = 3 的時候 return 404 page --> 適合靜態網站
 //? blocking: 與 true 一樣不回傳 404 而是執行 getStaticProps ， 不一樣的點：!!!1. 沒有 router.isFallback!!!  !!!2.讓頁面卡在 getStaticProps 的階段!!!
 //?所以使用者體驗會非常像似 getServerSideProps ，但優點是下次使用者再次瀏覽同一個頁面時，伺服器可以直接回傳已經生成的 HTML 檔案，往後甚至可以藉由 CDN 的 cache 提升頁面的載入速度。
