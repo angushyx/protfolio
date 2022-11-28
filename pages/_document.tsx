8ca287baf877e6fc04840c8f3b32bfadb24a89a2
Angus <94235117+angushyx@users.noreply.github.com>
2022-11-28 10:59:01 +0800

    feat;add GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document"
+import Script from "next/script"
 
 export default function Document() {
   return (
@@ -10,12 +11,24 @@ export default function Document() {
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-
         <link
           href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Noto+Sans+TC:wght@400;500;700&family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-BQ1L1GT80L"
+        ></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-BQ1L1GT80L');
+        `}
+        </Script>
       </Head>
+
       <body>
         <Main />
         <NextScript />
